f15de92e37b957db7ef1edec6303b8e54a2a4ca2
Nick Van Doorn <vandoorn.nick@gmail.com>
2018-11-18 23:55:03 -0800

    Stub out firebase implementation

--- src/reporters/firebase/firebase.ts
+++ src/reporters/firebase/firebase.ts
@@ -1,3 +1,15 @@
-import { Reporter } from '../models/reporter.model'
+import { Reporter } from '../../models/reporter.model'
+import { Report } from '../../models/report.model'
+import { Context } from '../../models/context.model'
 
-export class Firebase implements Reporter {}
+export class Firebase implements Reporter {
+  name = 'Firebase Reporter'
+  constructor(private ctx: Context) {}
+  public record(r: Report): boolean {
+    return true
+  }
+
+  public async publish(): Promise<boolean> {
+    return true
+  }
+}
