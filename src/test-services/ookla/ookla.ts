268a443c96b21079e93196119b6272d833cff982
Nick Van Doorn <vandoorn.nick@gmail.com>
2018-11-18 23:54:35 -0800

    Stub out ookla implementation

--- src/test-services/ookla/ookla.ts
+++ src/test-services/ookla/ookla.ts
@@ -1,3 +1,16 @@
-import { TestService } from '../models/test-service.model'
+import { TestService } from '../../models/test-service.model'
+import { Context } from '../../models/context.model'
 
-export class Ookla implements TestService {}
+export class Ookla implements TestService {
+  name = 'Speedtest.net (Ookla) Speed Test'
+  constructor(private ctx: Context) {}
+  testDownload = async (): Promise<number> => {
+    return 5
+  }
+  testUpload = async (): Promise<number> => {
+    return 5
+  }
+  serverLocation = async (): Promise<string> => {
+    return 'Victoria, BC'
+  }
+}
