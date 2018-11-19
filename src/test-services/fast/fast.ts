f743ae86ca6cc1eba94d83f5c48394503fbf20d3
Nick Van Doorn <vandoorn.nick@gmail.com>
2018-11-18 23:53:34 -0800

    Stub out fast test service API

--- src/test-services/fast/fast.ts
+++ src/test-services/fast/fast.ts
@@ -1,3 +1,19 @@
-import { TestService } from '../models/test-service.model'
+import { TestService } from '../../models/test-service.model'
+import { Context } from '../../models/context.model'
+import fastTest from 'fast-speedtest-api'
 
-export class Fast implements TestService {}
+// TODO looks like none of the pre-implemented fast.com
+// modules do not support upload speed ;-;
+export class Fast implements TestService {
+  name = 'Fast.com Speed Test'
+  constructor(private ctx: Context) {}
+  testDownload = async (): Promise<number> => {
+    return 5
+  }
+  testUpload = async (): Promise<number> => {
+    return 5
+  }
+  serverLocation = async (): Promise<string> => {
+    return 'Victoria, BC'
+  }
+}
