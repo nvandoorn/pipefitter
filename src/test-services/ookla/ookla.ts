3bf6bebec947b9b0c389a1fce4fa55afd42c6753
Nick Van Doorn <vandoorn.nick@gmail.com>
2018-11-25 23:50:29 -0800

    Wrap speedtest-net module

--- src/test-services/ookla/ookla.ts
+++ src/test-services/ookla/ookla.ts
@@ -1,16 +1,27 @@
 import { TestService } from '../../models/test-service.model'
 import { Context } from '../../models/context.model'
+import st from 'speedtest-net'
+
+const test = (): Promise<any> =>
+  new Promise((resolve, reject) => {
+    st({ maxTime: 5000 }).on('data', data => {
+      resolve(data)
+    })
+  })
 
 export class Ookla implements TestService {
   name = 'Speedtest.net (Ookla) Speed Test'
   constructor(private ctx: Context) {}
   testDownload = async (): Promise<number> => {
-    return 5
+    const testData = await test()
+    return testData.speeds.download
   }
   testUpload = async (): Promise<number> => {
-    return 5
+    const testData = await test()
+    return testData.speeds.upload
   }
   serverLocation = async (): Promise<string> => {
-    return 'Victoria, BC'
+    const testData = await test()
+    return testData.server.location
   }
 }
